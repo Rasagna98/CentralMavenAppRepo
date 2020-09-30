24b4de1e728ed94b584e29986eee9a81dbd06f69
Rasagna98 <rasagna.yadagani@gmail.com>
2020-09-30 18:22:15 +0530

    Commit AppTest

--- src/main/java/com/cg/project/App.java
+++ src/main/java/com/cg/project/App.java
@@ -4,9 +4,14 @@ package com.cg.project;
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static void mains( String[] args )
     {
         System.out.println( "Hello World!" );
     }
